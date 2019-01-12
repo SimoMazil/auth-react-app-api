2989f1e71c2e54e2b209de2aecac5d7313dfefc0
Simo Mazil <mohamed.mazil@gmail.com>
2019-01-12 16:03:38 +0100

    if book not found send error result

--- src/routes/books.js
+++ src/routes/books.js
@@ -9,18 +9,22 @@ router.use(authenticate)
 router.get("/search", (req, res) => {
   request.get(`https://www.goodreads.com/search/index.xml?key=S1ROvvSssa0V1HUXoF0yg&q=${req.query.q}`).then(
     result => parseString(result, (err, goodreadsResult) => {
-      res.json({
-        books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
-          work => ({
-            goodreadsId: work.best_book[0].id[0]._,
-            title: work.best_book[0].title[0],
-            authors: work.best_book[0].author[0].name[0],
-            covers: [
-              work.best_book[0].image_url[0]
-            ]
-          })
-        )
-      })
+      if(goodreadsResult.GoodreadsResponse.search[0].results[0].work) {
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [
+                work.best_book[0].image_url[0]
+              ]
+            })
+          )
+        })
+      } else {
+        res.status(404).json({errors: {global: "Book not found"}})
+      }
     })
   )
 })
